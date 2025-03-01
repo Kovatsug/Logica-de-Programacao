8ec9925471bfa85e6dd3109eea9d7368116aa595
Kovatsug <gustavokreuch40@gmail.com>
2025-03-01 10:11:24 -0300

    Add files via upload

---
+++ GHK-LP-02-EX-04.py
@@ -0,0 +1,5 @@
+largura=float(input("Informe a Largura do terreno em metros:"))
+profundidade =float(input("Informe a profindidade do terreno em metros:"))
+area= largura*profundidade
+hectare=area/10000
+print("Seu terreno tem:", hectare, "hectares")
