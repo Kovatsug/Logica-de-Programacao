533a1bafd1a3c630fe83ebac35212a67fe4128c5
Kovatsug <gustavokreuch40@gmail.com>
2025-03-01 10:17:54 -0300

    Delete GHK-LP-02-EX-04.py

--- GHK-LP-02-EX-04.py
+++
@@ -1,5 +0,0 @@
-largura=float(input("Informe a Largura do terreno em metros:"))
-profundidade =float(input("Informe a profindidade do terreno em metros:"))
-area= largura*profundidade
-hectare=area/10000
-print("Seu terreno tem:", hectare, "hectares")
