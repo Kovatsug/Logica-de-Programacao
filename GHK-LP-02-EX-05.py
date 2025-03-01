18643e1923609e3ab0389cbf2a61964c97f05d32
Kovatsug <gustavokreuch40@gmail.com>
2025-03-01 10:18:01 -0300

    Delete GHK-LP-02-EX-05.py

--- GHK-LP-02-EX-05.py
+++
@@ -1,7 +0,0 @@
-vasilhames_pequenos = int(input("Digite a quantidade de vasilhames de um litro ou menos: "))
-
-vasilhames_grandes = int(input("Digite a quantidade de vasilhames de mais de um litro: "))
-
-total_credito = vasilhames_pequenos * 0.10 + vasilhames_grandes * 0.25
-
-print("O valor total dos créditos é: R$ {:.2f}".format(total_credito))
